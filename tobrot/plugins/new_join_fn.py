cfe608502d14797c5874bbd3148553c274f2536d
evilbros7 <69081291+evilbros7@users.noreply.github.com>
2020-09-29 11:27:10 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,8 +38,7 @@ async def help_message_f(client, message):
     #message_id = 99
     # display the /help
     
-    await message.reply_text("""⚡ JOIN TORRENT MIRROR  GROUP\n\n 👉Link: <a href="https://t.me/MaxxMirror">MAXX MIRROR GROUP</a>""", disable_web_page_preview=True)
-
+    await message.reply_text("""✨join this Channel for Premium Contents✨-- @premiumcoursesdrive\n\n And also don't forget to Read this 😈: <a href="https://t.me/FreeTorrentDownloader/101478">Pinned Message</a>""", disable_web_page_preview=True)
 
 async def rename_message_f(client, message):
     inline_keyboard = []
